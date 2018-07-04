9ae13e585309af447afb7d94ea730b010038269d
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-03 18:23:22 -0700

    Implemented code for saving issue data to Local Storage after form submit

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue); // event handler to submit on form
+
+// A reference to the form element is retrieved by using getElementById. We’re passing in the string issueInputForm which is the id of the <form> element. The addEventListener method is called to attach the submit event of the form to the saveIssue event handler function. Let's implement this function here:
+function saveIssue(e) {
+    var issueId = chance.guid();
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueStatus = 'Open';
+    var issue = {
+      id: issueId,
+      description: issueDesc,
+      severity: issueSeverity,
+      assignedTo: issueAssignedTo,
+      status: issueStatus
+    }
+    
+    if (localStorage.getItem('issues') === null) {
+      var issues = [];
+      issues.push(issue);
+      localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+      var issues = JSON.parse(localStorage.getItem('issues'));
+      issues.push(issue);
+      localStorage.setItem('issues', JSON.stringify(issues));
+    }
+    
+    document.getElementById('issueInputForm').reset();
+   
+    fetchIssues();
+    
+    e.preventDefault(); 
+  }
+
 function fetchIssues () {
-    var issues = JSON.parse(localStorage.getItem('issues'));
-    var issuesList = document.getElementById('issuesList');
+    var issues = JSON.parse(localStorage.getItem('issues')); //this 1st line retrieves issues from local storage and then parsing the string result into a JSON object
+    var issuesList = document.getElementById('issuesList'); // This line retrieves the reference to the <div> element with ID 'issuesList'. HTML content of the element can be accessed by property 'innerHTML'. That property is set to the content of an empty string. Next it's looping over elements in issues by using a 'for loop' and adding HTML outputs for the elements to 'issuesList.innerHTML'.
     
     issuesList.innerHTML = '';
     
